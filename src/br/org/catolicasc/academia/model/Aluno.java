235ac3749b7e163e52066206f6c8afc94ef1782f
Felipe Rubel Borges <felipe.rubel@hotmail.com>
2016-06-06 20:12:17 -0300

    Adicionado as classes de modelagem do projeto da Academia

---
+++ src/br/org/catolicasc/academia/model/Aluno.java
@@ -0,0 +1,46 @@
+package br.org.catolicasc.academia.model;
+
+import java.util.Date;
+
+public class Aluno {
+	
+	private long id;
+	private String nome;
+	private String endereco;
+	private int cpf;
+	private Date dtNascimento;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDtNascimento() {
+		return dtNascimento;
+	}
+	public void setDtNascimento(Date dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+	
+	
+
+}
---
+++ src/br/org/catolicasc/academia/model/Matricula.java
@@ -0,0 +1,5 @@
+package br.org.catolicasc.academia.model;
+
+public class Matricula {
+
+}
