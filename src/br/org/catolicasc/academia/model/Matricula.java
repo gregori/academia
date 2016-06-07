340ffc2e75e18297228e9110fa1cfca26b26e113
eduardobarbiero <eduardombarbiero@gmail.com>
2016-06-07 19:40:23 -0300

    Adiocionado notations em Matricula

--- src/br/org/catolicasc/academia/model/Matricula.java
+++ src/br/org/catolicasc/academia/model/Matricula.java
@@ -3,15 +3,24 @@ package br.org.catolicasc.academia.model;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Matricula {
 	
+	@Id
+	@GeneratedValue
 	private Long id;
 	private Date data;
 	private Date validade;
-	private Plano plano;
+	
+	@ManyToOne
 	private Aluno aluno;
+	
+	@ManyToOne
+	private Plano plano;
 
 	public Long getId() {
 		return id;
