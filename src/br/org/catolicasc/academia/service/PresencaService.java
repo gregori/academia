da753d69ea21a3677ae16f556574bec34298a20a
Douglas Wille <willedouglas10@gmail.com>
2016-06-07 19:57:43 -0300

    Implementado DAO e Implementação / Service

--- src/br/org/catolicasc/academia/service/PresencaService.java
+++ src/br/org/catolicasc/academia/service/PresencaService.java
@@ -1,13 +1,14 @@
-package br.org.catolicasc.academia.dao;
+package br.org.catolicasc.academia.service;
 
 import java.util.Date;
 import java.util.List;
 
 import br.org.catolicasc.academia.model.Presenca;
 
-public interface PresencaDAO {
+public interface PresencaService {
 	Presenca findById(Long id);
 	void save(Presenca presenca);
+	void update(Presenca presenca);
 	List<Presenca> findAll();
 	List<Presenca> findPresencaByDate(Date dtInicial, Date dtFinal);
 }
