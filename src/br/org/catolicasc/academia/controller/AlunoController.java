9a0bf2f485031de1e2d72049baa81328a5b81448
Rodrigo Habib Gregori <rodrigo.gregori@gmail.com>
2016-06-07 18:06:15 -0300

    Added AlunoController - commented out PagamentoController for testing

---
+++ src/br/org/catolicasc/academia/controller/AlunoController.java
@@ -0,0 +1,100 @@
+package br.org.catolicasc.academia.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.org.catolicasc.academia.model.Aluno;
+import br.org.catolicasc.academia.service.AlunoService;
+
+@Controller
+@RequestMapping("/aluno")
+public class AlunoController {
+	@Autowired
+	AlunoService service;
+	
+	@Autowired
+	MessageSource messageSource;
+	
+	/*
+	 * Este método lista todos os alunos
+	 */
+	@RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
+	public String listaAlunos(Model model) {
+		List<Aluno> alunos = service.findAll();
+		model.addAttribute("alunos", alunos);
+		return "aluno/lista";
+	}
+	
+	/*
+	 * Este método mostra o formulário para adição de usuários
+	 */
+	@RequestMapping(value = { "/new" }, method = RequestMethod.GET)
+	public String novoAlunoForm(Model model) {
+		Aluno aluno = new Aluno();
+		model.addAttribute("aluno", aluno);
+		model.addAttribute("edicao", false);
+		return "aluno/formAluno";
+	}
+	
+	/*
+	 * Este método será chamado ao enviar o formulário de criação
+	 * de usuário, lidando com o método POST
+	 */
+	@RequestMapping(value = { "/new" }, method = RequestMethod.POST)
+	public String gravaAluno(@Valid Aluno aluno, BindingResult resultado,
+			Model model) {
+		if (resultado.hasErrors()) {
+			return "aluno/formAluno";
+		}
+		
+		service.save(aluno);
+		model.addAttribute("sucesso", "Aluno " + aluno.getNome() + " cadastrado com sucesso!");
+		return "aluno/sucesso";
+	}
+	
+	/*
+	 * Este método mostra o formulário de edição de usuário
+	 */
+	@RequestMapping(value = { "/edit/{id}" }, method = RequestMethod.GET)
+	public String editaAlunoForm(@PathVariable Long id, Model model) {
+		Aluno aluno = service.findById(id);
+		model.addAttribute("aluno", aluno);
+		model.addAttribute("edicao", true);
+		return "aluno/formAluno";
+	}
+	
+	/*
+	 * Esse chamado lida com a submissão POST da edição de usuário
+	 */
+	@RequestMapping(value = { "/edit/{id}" }, method = RequestMethod.POST)
+	public String gravaAlunoEdicao(@Valid Aluno aluno, @PathVariable Long id,
+			BindingResult resultado, Model model) {
+		if (resultado.hasErrors()) {
+			return "aluno/formAluno";
+		}
+		service.update(aluno);
+		model.addAttribute("sucesso", "Aluno " + aluno.getNome() + " foi atualizado com sucesso.");
+		return "aluno/sucesso";
+	}
+	
+	/*
+	 * Este método vai apagar um aluno pelo seu CPF (podia ser pelo ID também)
+	 */
+	@RequestMapping(value = { "/delete/{cpf}" }, method = RequestMethod.GET)
+	public String apagaAluno(@PathVariable int cpf) {
+		service.deleteAlunoByCPF(cpf);
+		return "redirect:/aluno/list";
+	}
+	
+	
+}
--- src/br/org/catolicasc/academia/controller/PagamentoController.java
+++ src/br/org/catolicasc/academia/controller/PagamentoController.java
@@ -21,99 +21,99 @@ import br.org.catolicasc.academia.model.Pagamento;
 
  
 @Controller
-@RequestMapping("/")
+@RequestMapping("/pagamento")
 public class PagamentoController {
  
-    @Autowired
-    PagamentoService service;
-     
-    @Autowired
-    MessageSource messageSource;
- 
-
-    @RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
-    public String listPagamentos(ModelMap model) {
- 
-        List<Pagamento> pagamentos = service.findAllPagamentos();
-        model.addAttribute("pagamento", pagamentos);
-        return "allpagamentos";
-    }
- 
-
-    @RequestMapping(value = { "/new" }, method = RequestMethod.GET)
-    public String newPagamento(ModelMap model) {
-        Pagamento pagamentos = new Pagamento();
-        model.addAttribute("pagamento", pagamentos);
-        model.addAttribute("edit", false);
-        return "registration";
-    }
- 
-    
-    @RequestMapping(value = { "/new" }, method = RequestMethod.POST)
-    public String savePagamento(@Valid Pagamento pagamentos, BindingResult result,
-            ModelMap model) {
- 
-        if (result.hasErrors()) {
-            return "registration";
-        }
- 
-
-        if(!service.isPagamentoidUnique(pagamentos.getId(), pagamentos.getId())){
-            FieldError idError =new FieldError("pagamento","id",messageSource.getMessage("non.unique.id", new Long[]{pagamentos.getId()}, Locale.getDefault()));
-            result.addError(idError);
-            return "registration";
-        }
-         
-        service.savePagamento(pagamentos);
- 
-        model.addAttribute("success", "Pagamento " + pagamentos.getAluno() + " registered successfully");
-        return "success";
-    }
- 
- 
-    /*
-     * This method will provide the medium to update an existing Pagamento.
-     */
-    @RequestMapping(value = { "/edit-{id}-pagamento" }, method = RequestMethod.GET)
-    public String editPagamento(@PathVariable String id, ModelMap model) {
-        Pagamento pagamentos = service.findPagamentoByid(id);
-        model.addAttribute("Pagamento", pagamentos);
-        model.addAttribute("edit", true);
-        return "registration";
-    }
-     
-    /*
-     * This method will be called on form submission, handling POST request for
-     * updating Pagamento in database. It also validates the user input
-     */
-    @RequestMapping(value = { "/edit-{id}-Pagamento" }, method = RequestMethod.POST)
-    public String updatePagamento(@Valid Pagamento Pagamento, BindingResult result,
-            ModelMap model, @PathVariable String id) {
- 
-        if (result.hasErrors()) {
-            return "registration";
-        }
- 
-        if(!service.isPagamentoidUnique(Pagamento.getId(), Pagamento.getId())){
-            FieldError idError =new FieldError("Pagamento","id",messageSource.getMessage("non.unique.id", new Long[]{Pagamento.getId()}, Locale.getDefault()));
-            result.addError(idError);
-            return "registration";
-        }
- 
-        service.updatePagamento(Pagamento);
- 
-        model.addAttribute("success", "Pagamento " + Pagamento.getAluno()  + " updated successfully");
-        return "success";
-    }
- 
-     
-    /*
-     * This method will delete an Pagamento by it's id value.
-     */
-    @RequestMapping(value = { "/delete-{id}-Pagamento" }, method = RequestMethod.GET)
-    public String deletePagamento(@PathVariable String id) {
-        service.deletePagamentoByid(id);
-        return "redirect:/list";
-    }
+//    @Autowired
+//    PagamentoService service;
+//     
+//    @Autowired
+//    MessageSource messageSource;
+// 
+//
+//    @RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
+//    public String listPagamentos(ModelMap model) {
+// 
+//        List<Pagamento> pagamentos = service.findAllPagamentos();
+//        model.addAttribute("pagamento", pagamentos);
+//        return "allpagamentos";
+//    }
+// 
+//
+//    @RequestMapping(value = { "/new" }, method = RequestMethod.GET)
+//    public String newPagamento(ModelMap model) {
+//        Pagamento pagamentos = new Pagamento();
+//        model.addAttribute("pagamento", pagamentos);
+//        model.addAttribute("edit", false);
+//        return "registration";
+//    }
+// 
+//    
+//    @RequestMapping(value = { "/new" }, method = RequestMethod.POST)
+//    public String savePagamento(@Valid Pagamento pagamentos, BindingResult result,
+//            ModelMap model) {
+// 
+//        if (result.hasErrors()) {
+//            return "registration";
+//        }
+// 
+//
+//        if(!service.isPagamentoidUnique(pagamentos.getId(), pagamentos.getId())){
+//            FieldError idError =new FieldError("pagamento","id",messageSource.getMessage("non.unique.id", new Long[]{pagamentos.getId()}, Locale.getDefault()));
+//            result.addError(idError);
+//            return "registration";
+//        }
+//         
+//        service.savePagamento(pagamentos);
+// 
+//        model.addAttribute("success", "Pagamento " + pagamentos.getAluno() + " registered successfully");
+//        return "success";
+//    }
+// 
+// 
+//    /*
+//     * This method will provide the medium to update an existing Pagamento.
+//     */
+//    @RequestMapping(value = { "/edit-{id}-pagamento" }, method = RequestMethod.GET)
+//    public String editPagamento(@PathVariable String id, ModelMap model) {
+//        Pagamento pagamentos = service.findPagamentoByid(id);
+//        model.addAttribute("Pagamento", pagamentos);
+//        model.addAttribute("edit", true);
+//        return "registration";
+//    }
+//     
+//    /*
+//     * This method will be called on form submission, handling POST request for
+//     * updating Pagamento in database. It also validates the user input
+//     */
+//    @RequestMapping(value = { "/edit-{id}-Pagamento" }, method = RequestMethod.POST)
+//    public String updatePagamento(@Valid Pagamento Pagamento, BindingResult result,
+//            ModelMap model, @PathVariable String id) {
+// 
+//        if (result.hasErrors()) {
+//            return "registration";
+//        }
+// 
+//        if(!service.isPagamentoidUnique(Pagamento.getId(), Pagamento.getId())){
+//            FieldError idError =new FieldError("Pagamento","id",messageSource.getMessage("non.unique.id", new Long[]{Pagamento.getId()}, Locale.getDefault()));
+//            result.addError(idError);
+//            return "registration";
+//        }
+// 
+//        service.updatePagamento(Pagamento);
+// 
+//        model.addAttribute("success", "Pagamento " + Pagamento.getAluno()  + " updated successfully");
+//        return "success";
+//    }
+// 
+//     
+//    /*
+//     * This method will delete an Pagamento by it's id value.
+//     */
+//    @RequestMapping(value = { "/delete-{id}-Pagamento" }, method = RequestMethod.GET)
+//    public String deletePagamento(@PathVariable String id) {
+//        service.deletePagamentoByid(id);
+//        return "redirect:/list";
+//    }
  
 }
