2637dfde812048609dc7964f30921393b98a3a0a
Douglas Wille <willedouglas10@gmail.com>
2016-06-06 21:09:37 -0300

    Implentado findAll na classe abstrata. Ajuste do PagamentoDAO

--- src/br/org/catolicasc/academia/dao/AbstractDao.java
+++ src/br/org/catolicasc/academia/dao/AbstractDao.java
@@ -3,42 +3,49 @@ package br.org.catolicasc.academia.dao;
 import java.io.Serializable;
 
 import java.lang.reflect.ParameterizedType;
- 
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
- 
+
 public abstract class AbstractDao<PK extends Serializable, T> {
-     
-    private final Class<T> persistentClass;
-     
-    @SuppressWarnings("unchecked")
-    public AbstractDao(){
-        this.persistentClass =(Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
-    }
-     
-    @Autowired
-    private SessionFactory sessionFactory;
- 
-    protected Session getSession(){
-        return sessionFactory.getCurrentSession();
-    }
- 
-    public T getByKey(PK key) {
-        return (T) getSession().get(persistentClass, key);
-    }
- 
-    public void persist(T entity) {
-        getSession().persist(entity);
-    }
- 
-    public void delete(T entity) {
-        getSession().delete(entity);
-    }
-     
-    protected Criteria createEntityCriteria(){
-        return getSession().createCriteria(persistentClass);
-    }
- 
+
+	private final Class<T> persistentClass;
+
+	@SuppressWarnings("unchecked")
+	public AbstractDao() {
+		this.persistentClass = (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass())
+				.getActualTypeArguments()[1];
+	}
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	protected Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+
+	public T getByKey(PK key) {
+		return (T) getSession().get(persistentClass, key);
+	}
+
+	public void persist(T entity) {
+		getSession().persist(entity);
+	}
+
+	public void delete(T entity) {
+		getSession().delete(entity);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> findAll(T entity) {
+		return (List<T>) createEntityCriteria().list();
+	}
+
+	protected Criteria createEntityCriteria() {
+		return getSession().createCriteria(persistentClass);
+	}
+
 }
