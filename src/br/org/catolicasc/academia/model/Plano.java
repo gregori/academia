5aee288747fddf51ab0d39651b2a7cfcd7c89ea7
Rodrigo Habib Gregori <rodrigo.gregori@gmail.com>
2016-06-07 21:45:56 -0300

    Corrected Plano

--- src/br/org/catolicasc/academia/model/Plano.java
+++ src/br/org/catolicasc/academia/model/Plano.java
@@ -1,16 +1,31 @@
 package br.org.catolicasc.academia.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Plano {
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
+	
+	@NotNull
 	private String nome;
+	@NotNull
 	private String descricao;
 	private double valor;
+	@NotNull
 	private int aulas;
 
+	@OneToMany(mappedBy="plano")
+	private List<Matricula> matriculas;
+	
 	public long getId() {
 		return this.id;
 	}
