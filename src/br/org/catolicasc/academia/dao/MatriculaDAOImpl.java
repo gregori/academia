bb4dea4d126e4642f64a35cfd860a98a908931bc
eduardobarbiero <eduardombarbiero@gmail.com>
2016-06-13 21:28:52 -0300

    Feito implements e services faltantes para Matricula

---
+++ src/br/org/catolicasc/academia/dao/MatriculaDAOImpl.java
@@ -0,0 +1,46 @@
+package br.org.catolicasc.academia.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.CriteriaSpecification;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+import br.org.catolicasc.academia.model.Aluno;
+import br.org.catolicasc.academia.model.Matricula;
+import br.org.catolicasc.academia.model.Presenca;
+
+@Repository("alunoDao")
+public class MatriculaDAOImpl extends AbstractDao<Long, Matricula> implements MatriculaDAO {
+
+	@Override
+	public Matricula findById(Long id) {
+		return getByKey(id);
+	}
+
+	@Override
+	public void delete(Long id) {
+		Query query = getSession().createSQLQuery("delete from Matricula where id = :id");
+		query.setLong("id", id);
+		query.executeUpdate();
+	}
+	
+	@Override
+	public void save(Matricula matricula) {
+		persist(matricula);
+	}
+	
+	@Override
+	public List<Matricula> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public void update(Matricula matricula) {
+		persist(matricula);
+	}
+
+}
--- src/br/org/catolicasc/academia/service/MatriculaService.java
+++ src/br/org/catolicasc/academia/service/MatriculaService.java
@@ -6,9 +6,9 @@ import br.org.catolicasc.academia.model.Aluno;
 import br.org.catolicasc.academia.model.Matricula;
 
 public interface MatriculaService {
-	Matricula findById(int id);
+	Matricula findById(Long id);
 	void save(Matricula matricula);
 	void update(Matricula matricula);
-	void delete(int id);
+	void delete(Long id);
 	List<Matricula> findAll();
 }
